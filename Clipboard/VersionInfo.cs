8a6ee438b57d1243fe0087363d0916672ef08a5a
pengcunfu <218579384+pengcunfu@users.noreply.github.com>
2026-08-26 20:45:33 +0800

    杂项: 版本号升至 1.0.0.17

--- Clipboard/VersionInfo.cs
+++ Clipboard/VersionInfo.cs
@@ -1,10 +1,10 @@
-﻿namespace ClipboardApp;
+namespace ClipboardApp;
 
 public static class VersionInfo
 {
     public const string Version = "1.0.0";
-    public const int BuildNumber = 16;
-    public const string BuiltAt = "2026-08-21 00:00:51";
+    public const int BuildNumber = 17;
+    public const string BuiltAt = "2026-08-26 20:34:16";
 
     public static string BuildVersion =>
         BuildNumber > 0 ? $"{Version}.{BuildNumber}" : string.Empty;
