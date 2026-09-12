494c632739fb581a947c1d1dfcfe5e28cc277f51
pengcunfu <3173484026@qq.com>
2026-09-13 03:58:36 +0800

    杂项: 版本号升至 1.1.0

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -2,9 +2,9 @@ namespace ClipboardApp;
 
 public static class VersionInfo
 {
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.0";
     public const int BuildNumber = 0;
-    public const string BuiltAt = "2026-08-31 16:36:32";
+    public const string BuiltAt = "2026-09-13 03:58:22";
 
     public static string BuildVersion =>
         BuildNumber > 0 ? $"{Version}.{BuildNumber}" : string.Empty;
